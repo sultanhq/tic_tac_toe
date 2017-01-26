9dc144f93786afc4007ade9621de9b294015a237
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-26 10:30:25 +0000

    updated game test

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -4,9 +4,10 @@ require './lib/game.rb'
 describe 'tictactoe' do
   let(:player) { double('player') }
   let(:player2) { double('player2') }
+  let(:board) { double('board', rows: 3) }
 
   it 'expects a new game to be instantited with 2 players' do
-    game = Game.new(player, player2)
+    game = Game.new(player, player2, board)
     expect(game.players.length).to eq 2
   end
 
