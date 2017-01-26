d9445332e331e7b3730ac6ba35fcb645615501d1
Rob Adams <robert.andrew.adams@gmail.com>
2017-01-26 11:12:08 +0000

    added test for adding a 0

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -10,13 +10,18 @@ describe 'board' do
   end
 
   it 'expects a board to accept an X in one of its cells' do
-    @board.place_marker("X",2,2)
+    @board.place_marker('X', 2, 2)
     expect(@board.grid[2][2]).to eq 'X'
   end
 
-  it 'expects a board not to accept an X in one of its cells if already populated' do
-    @board.place_marker("X",2,2)
-    message = "marker already placed in that cell"
-    expect{@board.place_marker("X",2,2)}.to raise_error(message)
+  it 'expects a board to accept an 0 in one of its cells' do
+    @board.place_marker('0', 1, 2)
+    expect(@board.grid[1][2]).to eq '0'
+  end
+
+  it 'expects a board not to accept an X in a cell if already populated' do
+    @board.place_marker('X', 2, 2)
+    message = 'marker already placed in that cell'
+    expect{ @board.place_marker('X', 2, 2) }.to raise_error(message)
   end
 end
